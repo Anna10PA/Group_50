08839b6cc91b153f7cbc638d8646d7b6dafecb6c
Anna10PA <puturidzeana0210@gmail.com>
2024-10-10 21:39:15 +0400

    commit

---
+++ day 32/Homework/Homework_1.py
@@ -0,0 +1,17 @@
+"""  1) შექმენით რიცხვებით სავსე სია, შემდეგ დაწერეთ კოდი რომელიც დაბეჭდავს ამ სიიდან ყველაზე უდიდეს რიცხვს გამოგადგებათ for loop ი კარგად დაფიქრდით და გაიაზრეთ"""
+
+list = [1, 200, 3.1, 4, 5, 0, 15257, 16, -12, -25.9]
+
+# max ricxvi
+max = list[0]
+for i in range(len(list)):
+    if max < list[i]:
+        max = list[i]
+print(max)
+
+# min ricxvi
+min = list[0]
+for i in range(len(list)):
+    if min > list[i]:
+        min = list[i]
+print(min)
