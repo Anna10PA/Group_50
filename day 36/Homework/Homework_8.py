2ee391ce82746458710229e6f3c28917b804f935
Anna10PA <puturidzeana0210@gmail.com>
2024-10-24 23:01:09 +0400

    commit

--- day 36/Homework/Homework_8.py
+++ day 36/Homework/Homework_8.py
@@ -1,12 +1,12 @@
 '''8. დაწერე ფუნქცია, რომელიც იღებს რიცხვს და ამოწმებს, არის თუ არა ის მარტივი რიცხვი.'''
 
-user_number = int(input("Enter number: "))
+# user_number = int(input("Enter number: "))
 
-def check(num):
-    if num > 1:
-        for i in range(2, 10):
-            if num % i == 0 and num != i:
-                return f"რიცხვი {num} არ არის მარტივი"
-            else:
-                return f'რიცხვი {num} არის მარტივი'
-print(check(user_number))
+# def check(num):
+#     if num > 1:
+#         for i in range(2, 10):
+#             if num % i == 0 and num != i:
+#                 return f"რიცხვი {num} არ არის მარტივი"
+#             else:
+#                 return f'რიცხვი {num} არის მარტივი'
+# print(check(user_number))
