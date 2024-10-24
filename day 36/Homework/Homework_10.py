e2a851be3aa00747619b9414db15c78caf5279c2
Anna10PA <puturidzeana0210@gmail.com>
2024-10-24 23:34:15 +0400

    commit

--- day 36/Homework/Homework_10.py
+++ day 36/Homework/Homework_10.py
@@ -1,7 +1,7 @@
 '''10. დაწერე ფუნქცია, რომელიც იღებს სიას და აბრუნებს მასში მხოლოდ მარტივ რიცხვებს. '''
 
 user_number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-new_list =[]
+new_list = []
 
 def check(number):
     if number > 1:
@@ -12,6 +12,6 @@ def check(number):
     return None
 
 for num in user_number_list:
-    if check(num):
+    if check(num) == True:
         new_list.append(num)
 print(new_list)
