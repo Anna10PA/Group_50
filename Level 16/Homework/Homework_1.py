68e5669e843da6df1d06edfd4b32c4a81826e616
Anna10PA <puturidzeana0210@gmail.com>
2024-11-13 21:32:13 +0400

    commit

---
+++ Level 16/Homework/Homework_1.py
@@ -0,0 +1,8 @@
+'''level 16:
+     1) დაბეჭდეთ 1 დან 50 მდე რიცხვები while loop ის გამოყენებით'''
+
+num = 1
+
+while num <= 50:
+    print(num)
+    num = num + 1
---
+++ Level 16/Homework/Homework_2.py
@@ -0,0 +1,7 @@
+'''     2) დაბეჭდეთ 1 დან 50 მდე მხოლოდ ლუწი რიცხვები if else ის გამოყენების გარეშე
+'''
+number = 0
+
+while number <= 50:
+    print(number)
+    number = number + 2
