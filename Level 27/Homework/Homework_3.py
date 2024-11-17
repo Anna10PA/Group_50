41120a37c74c0db1054c61d166c511339d507c51
Anna10PA <puturidzeana0210@gmail.com>
2024-11-17 12:45:22 +0400

    commit

--- Level 27/Homework/Homework_3.py
+++ Level 27/Homework/Homework_3.py
@@ -3,6 +3,10 @@
 num1 = int(input("Enter 1st num: "))
 num2 = int(input('Enter 2nd num: '))
 
-for i in range(num1, num2+1):
-    print(num1 * num2)
-    print(num1 + num2)
+for num_1 in range(num1 + 1):
+    for num_2 in range(num2 + 2):
+        print(str(num_1) + ' + ' + str(num_2) + ' = ' + str(num_1 + num_2))
+
+for num_1 in range(num1 + 1):
+    for num_2 in range(num2 + 2):
+        print(str(num_1) + ' * ' + str(num_2) + ' = ' + str(num_1 * num_2))
