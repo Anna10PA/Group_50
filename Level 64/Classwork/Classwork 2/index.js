d9cbcef06bde38fbc24f9c8f08befad36f88e611
Anna10PA <puturidzeana0210@gmail.com>
2025-02-15 00:34:43 +0400

    commit

---
+++ Level 64/Classwork/Classwork 2/index.js
@@ -0,0 +1,16 @@
+// classwork 2.1
+let Name = prompt('Enter Your Name: ')
+let lastName = prompt("Enter your lastname: ")
+alert(Name +' '+ lastName )
+
+// classwork 2.2
+let number1 = prompt("enter your number: ")
+let number2 = prompt("enter your number: ")
+alert(number1 + number2)
+alert(number1 - number2)
+alert(number1 / number2)
+alert(number1 * number2)
+
+// classwork 2.3
+let userName = prompt("Enter your name: ")
+alert('Welcome '+ userName)
---
+++ Level 64/Homework/homework 1/index.js
@@ -0,0 +1,13 @@
+//     1) შექმენით 4 ცვლადი თითოეულ მათგანს მნიშვნელობად მიანიჭეთ განსხავებული რიცხვები შემდეგ კი ამ რიცხვებით რომლებსაც ინახავთ ცვლადებში ყველა შესაძლო კომბინაციით შეასრულეთ ოთხი მათემატიკური მოქმედება: მიმატება, გამოკლება, გამრავლება, გაყოფა ისე რომ დაიბეჭდოს თვითონ მოქმედება და პასუხი მაგ. ავიღოთ ორი რიცხვი 3 და 4 ამ ორი რიცხვის შემთხვევაში შედექი იქნება 3 + 4 = 7, 3 - 4 = -1 და ასე შემდეგ ყველა შესაძლო რიცხვების კომბინაციით და მოქმედებით
+ 
+
+let number1 = 12
+let number2 = 12.5
+let number3 = 10
+let number4 = 9
+
+
+console.log(number1 +' + '+ number2 +' + ' + number3 + ' + ' + number4 +' = ' + (number1 + number2 + number3 + number4))
+console.log(number1 +' - '+ number2 +' - ' + number3 + ' - ' + number4 +' = ' + (number1 - number2 - number3 - number4))
+console.log(number1 +' * '+ number2 +' * ' + number3 + ' * ' + number4 +' = ' + (number1 * number2 * number3 * number4))
+console.log(number1 +' / '+ number2 +' / ' + number3 + ' / ' + number4 +' = ' + (number1 / number2 / number3 / number4))
