836c200c67b50b3885d094100805d16ceb6ea021
Anna10PA <puturidzeana0210@gmail.com>
2025-02-17 17:52:59 +0400

    commit

---
+++ Level 66/Classwork/classwork 1/index.js
@@ -0,0 +1,11 @@
+//   1) შექმენით ლოგიკური ოპერატორების გამოყენებით ყველა შესაძლო კომბინაცია გამოიუყენეთ and და or ლოგიკური ოპერატორები, &&, ||
+
+// And
+console.log(true && true)
+console.log(true && false)
+console.log(false && false)
+
+// or
+console.log(true || true)
+console.log(true || false)
+console.log(false || false)
