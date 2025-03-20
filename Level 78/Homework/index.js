cb5e5522edf1f2c9991c4791aa885465c2b276e6
Anna10PA <puturidzeana0210@gmail.com>
2025-03-20 21:15:23 +0400

    commit

--- Level 78/Homework/index.js
+++ Level 78/Homework/index.js
@@ -1,7 +1,6 @@
-let myImages = 
-['https://cdn.britannica.com/96/176196-050-EFC5E6A6/Glock-pistol.jpg', 
-'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SIG_Pro_by_Augustas_Didzgalvis.jpg/1200px-SIG_Pro_by_Augustas_Didzgalvis.jpg',
-'https://www.shop.ge/files/09-2023/ad742650/sig-sauer-p365-pnevmaturi-870434418_large.jpg']
+let myImages = ['https://cdn.britannica.com/96/176196-050-EFC5E6A6/Glock-pistol.jpg', 
+'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SIG_Pro_by_Augustas_Didzgalvis.jpg/1200px-SIG_Pro_by_Augustas_Didzgalvis.jpg', 'https://www.shop.ge/files/09-2023/ad742650/sig-sauer-p365-pnevmaturi-870434418_large.jpg',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2GghbS6gij4B4TwBn8ejsd9AsmyUe73KWUQ&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTExcHLDgHrpN-7HDNIXlL3yBTZBOWuX6QzECIVHJRnrGYtSwpLZOK96twhxXrujWip9gA&usqp=CAU']
 
 let imageIndex = 0
 function change(){
@@ -10,7 +9,7 @@ function change(){
     result.innerHTML = ''
     let image = document.createElement('img')
 
-    if(imageIndex < (myImages.length)){
+    if(imageIndex < (myImages.length - 1)){
         imageIndex++
     }else{
         imageIndex = 0
