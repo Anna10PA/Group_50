88d904a15d8bdb2005b3f53a590d11dfc7cde349
Anna10PA <puturidzeana0210@gmail.com>
2026-03-07 22:28:15 +0400

    commit

---
+++ Level 219/Classwork/classwork.py
@@ -0,0 +1,9 @@
+# -- შექმენი table სახელად useraccount რომელსშიც შეძლებთ შემდეგი ინფორმაციების შეყვანას id, name, surname, age, email.
+
+# CREATE TABLE useraccount (
+#     id INTEGER UNIQUE,
+#     name VARCHAR(100),
+#     surname VARCHAR(100)
+#     age INTEGER
+#     email VARCHAR(100) UNIQUE
+# );
